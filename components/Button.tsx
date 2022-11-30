f9acdca4d08fb866c08843017859d9daac392fb2
William Mathewson <wncmathewson@me.com>
2022-11-30 22:20:03 +0000

    Remove IS_BROWSER switching on button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,11 +1,10 @@
 import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
 
 export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
     <button
       {...props}
-      disabled={!IS_BROWSER || props.disabled}
+      disabled={props.disabled}
       class="px-2 py-1 rounded border(gray-100 2) hover:bg-gray-200 disabled:bg-gray-200 disabled:pointer-events-none disabled:opacity-60"
     />
   );
