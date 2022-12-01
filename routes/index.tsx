08cdb163acdcbb96f2570eafb86e975eb89dd981
Will Mathewson <william.mathewson@freeagent.com>
2022-12-01 17:08:39 +0000

    Add preamble text to explain the website

--- routes/index.tsx
+++ routes/index.tsx
@@ -47,11 +47,35 @@ export default function Home({ data }: PageProps<Tokens | null>) {
             />
           )
           : (
-            <a class="flex justify-center py-5" href="/login">
-              <Button>
-                Log in to Spotify
-              </Button>
-            </a>
+            <div class="flex flex-col justify-center items-center mx-auto">
+              <div class="flex flex-col items-center gap-2 max-w-prose">
+                <p class="">
+                  This is a site to help visualise your saved albums by their release date. Spotify
+                  doesn't <em>currently</em>{" "}
+                  allow users to sort the albums in their collection by their release dates, only
+                  supporting sorting by when they were saved, alphabetically or by the artist.
+                </p>
+                <p>
+                  To remedy this, this site shows you your saved albums in descending order of
+                  release, with the latest albums being shown first. If you have a lot of saved
+                  albums, this may be a bit sluggish as the app has to download <strong>all</strong>
+                  {" "}
+                  of them to sort them. If you don't have many albums, this may be less helpful for
+                  you.
+                </p>
+                <p>
+                  <strong>
+                    If you have the Privacy Badger browser extension installed, you will need to
+                    disable it for this site!
+                  </strong>
+                </p>
+                <a class="flex justify-center my-5" href="/login">
+                  <Button>
+                    Log in to Spotify
+                  </Button>
+                </a>
+              </div>
+            </div>
           )}
       </div>
     </>
